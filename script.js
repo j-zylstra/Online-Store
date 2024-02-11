dab259ae74bcc44b3c909deefb381d7a5e17792e
Joe Zylstra <j-zylstra@hotmail.com>
2024-02-11 14:33:07 +0000

    "changed .pop()"

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ function renderProducts(type) {
 
     const productsElement = document.querySelector(`.${type}Products`);
     const path = window.location.pathname;
-    const productType = path.split('/').pop().replace('.html', '');
+    const productType = path.split('/');
     console.log(productType);
         fetch(`https://aqueous-ocean-91362-9acaca4dceea.herokuapp.com/products/type/${productType}`)
         .then(response => response.json())
