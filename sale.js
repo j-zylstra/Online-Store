38893e87c06df6c10c06ff8bcc6b0adffa7c4041
Joe Zylstra <j-zylstra@hotmail.com>
2024-03-11 14:27:23 +0000

    "adding domcontent loaded to sale logic"

--- sale.js
+++ sale.js
@@ -1,15 +1,12 @@
-const salePath = {
-    id: "sale",
-    url: "/sale"
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const salePath = {
+        id: "sale",
+        url: "/sale"
+    };
 
-const saleBtn = document.getElementById('sale');
+    const saleBtn = document.getElementById('sale');
 
-saleBtn.addEventListener('click', () => {
-    window.location.href = salePath.url;
-}); 
-
-if (window.location.href === salePath.url) {
-    renderSaleProducts();
-};
-
+    saleBtn.addEventListener('click', () => {
+        window.location.href = salePath.url;
+    });
+});
