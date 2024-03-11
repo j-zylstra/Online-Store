4173451e6a10fdec95ca393eef2c2b674a45a583
Joe Zylstra <j-zylstra@hotmail.com>
2024-03-11 14:47:32 +0000

    "moving all sale routing and product rendering to sale.js"

--- sale.js
+++ sale.js
@@ -9,5 +9,39 @@ document.addEventListener('DOMContentLoaded', () => {
     saleBtn.addEventListener('click', () => {
         window.location.href = salePath.url;
         console.log("new sale logic working");
+        renderSaleProducts();
     });
 });
+
+
+function renderSaleProducts() {
+
+    const saleProductsElement = document.querySelector(`.saleProducts`);
+
+        fetch(`https://aqueous-ocean-91362-9acaca4dceea.herokuapp.com/products/type/sale`)
+        .then(response => response.json())
+        .then(products => {
+            console.log(products);
+                products.forEach((product) => {
+                    
+                        const card = document.createElement("div");
+                        card.classList.add("card-border");
+                        card.innerHTML = `
+                            <img class="card" src="${product.imgsrc}" alt="">
+                            <h3>${product.name}</h3>
+                            <h2 id="old-price"><small>$</small>${product.oldprice}</h2>
+                            <h2 id="price"><small>$</small>${product.price}</h2>
+                            <button type="button" onclick="addToCart(${product.id})">Add To Cart</button>
+                        `;
+                        saleProductsElement.appendChild(card);
+                        
+                    } 
+                );
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
+
+    
+};
+
