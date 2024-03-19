a6b8bb940750893e01be4ac459f941c7e77b9be7
Joe Zylstra <j-zylstra@hotmail.com>
2024-03-19 10:25:35 +0000

    "adding render sale"

--- sale.js
+++ sale.js
@@ -1,34 +1,34 @@
 
-// function renderSaleProducts() {
+function renderSaleProducts() {
 
-//     const saleProductsElement = document.querySelector(`.saleProducts`);
+    const saleProductsElement = document.querySelector(`.saleProducts`);
 
-//         fetch(`https://aqueous-ocean-91362-9acaca4dceea.herokuapp.com/products/type/sale`)
-//         .then(response => response.json())
-//         .then(products => {
-//             console.log(products);
-//                 products.forEach((product) => {
+        fetch(`https://aqueous-ocean-91362-9acaca4dceea.herokuapp.com/products/type/sale`)
+        .then(response => response.json())
+        .then(products => {
+            console.log(products);
+                products.forEach((product) => {
                     
-//                         const card = document.createElement("div");
-//                         card.classList.add("card-border");
-//                         card.innerHTML = `
-//                             <img class="card" src="${product.imgsrc}" alt="">
-//                             <h3>${product.name}</h3>
-//                             <h2 id="old-price"><small>$</small>${product.oldprice}</h2>
-//                             <h2 id="price"><small>$</small>${product.price}</h2>
-//                             <button type="button" onclick="addToCart(${product.id})">Add To Cart</button>
-//                         `;
-//                         saleProductsElement.appendChild(card);
+                        const card = document.createElement("div");
+                        card.classList.add("card-border");
+                        card.innerHTML = `
+                            <img class="card" src="${product.imgsrc}" alt="">
+                            <h3>${product.name}</h3>
+                            <h2 id="old-price"><small>$</small>${product.oldprice}</h2>
+                            <h2 id="price"><small>$</small>${product.price}</h2>
+                            <button type="button" onclick="addToCart(${product.id})">Add To Cart</button>
+                        `;
+                        saleProductsElement.appendChild(card);
                         
-//                     } 
-//                 );
-//         })
-//         .catch(error => {
-//             console.log('Error:', error);
-//         });
+                    } 
+                );
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
 
     
-// };
+};
 
 
 
