d2c9b542437ea58834fabaeb794834a65c8852e0
Joe Zylstra <j-zylstra@hotmail.com>
2024-04-23 09:46:37 +0100

    "changing to old remove item from cart function"

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ function removeItemFromCart(id) {
 
      if (itemIndex !== -1) {
         
-         cart.splice(itemIndex);
+         cart.splice(itemIndex, 1);
     localStorage.setItem('cart', JSON.stringify(cart));
 
     
