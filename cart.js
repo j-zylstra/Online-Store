d53449d9404b17e2bf7b0656a7bfa451189403cb
Joe Zylstra <j-zylstra@hotmail.com>
2024-04-28 10:21:02 +0100

    "removing z-index: 1000"

--- cart.js
+++ cart.js
@@ -5,13 +5,4 @@ function redirect() {
     window.location.href = 'index.html';
 };
 
-// cartElement.addEventListener('click', function(event) {
-//     // Check if the clicked element is the close icon
-//     if (event.target.classList.contains('icon-close')) {
-//         // Get the product id from the data attribute of the parent element
-//         const productId = event.target.closest('.cart-item').getAttribute('data-product-id');
-//         console.log(productId); 
-//         removeItemFromCart(productId);
-//     }
-// });
 
